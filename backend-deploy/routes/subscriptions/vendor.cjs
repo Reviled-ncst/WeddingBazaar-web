e4fbd59ad7810bb1289a5136585a176a9a3eb6fe
Reviled-ncst <reviled.ncst@gmail.com>
2025-10-27 08:26:18 +0800

    Fix: Remove authentication requirement from modular subscription upgrade endpoint

--- backend-deploy/routes/subscriptions/vendor.cjs
+++ backend-deploy/routes/subscriptions/vendor.cjs
@@ -302,8 +302,9 @@ router.post('/create', authenticateToken, async (req, res) => {
 /**
  * PUT /api/subscriptions/vendor/upgrade
  * Upgrade vendor subscription to a higher tier
+ * NOTE: Authentication optional - vendor_id must be provided in request body
  */
-router.put('/upgrade', authenticateToken, async (req, res) => {
+router.put('/upgrade', async (req, res) => {
   try {
     const { vendor_id, new_plan } = req.body;
 
